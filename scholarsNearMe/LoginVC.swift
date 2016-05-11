e323f93b41240429e3f0e3addfdd2e317091766d
Eli Yazdi <eli6093@gmail.com>
2016-05-11 16:49:58 -0400

    removed sms, whatsapp, and phone

--- scholarsNearMe/LoginVC.swift
+++ scholarsNearMe/LoginVC.swift
@@ -14,17 +14,9 @@ class LoginVC: UIViewController, UINavigationControllerDelegate, UIImagePickerCo
     var addImage: UIButton!
     var firstNameTextFieldLabel: UILabel!
     var firstNameTextField: UITextField!
-    var phoneNumberTextFieldLabel: UILabel!
-    var phoneNumberTextField: UITextField!
     var forceTouchInformationLabel: UILabel!
-    var smsLabel: UILabel!
-    var smsSwitch: UISwitch!
-    var whatsAppLabel: UILabel!
-    var whatsAppSwitch: UISwitch!
     var loginButton: DeformationButton!
     var bounds = UIScreen.mainScreen().bounds
-    
-    var wrongPhoneNumber = false
     
     var UUID: String!
     
@@ -74,60 +66,14 @@ class LoginVC: UIViewController, UINavigationControllerDelegate, UIImagePickerCo
         firstName = firstNameTextField.text
     }
     
-    func editingDidBeginPhoneNumberTextFieldTextField(sender: UITextField!) {
-        phoneNumber = Int(phoneNumberTextField.text!)
-    }
     
-    func editingDidEndPhoneNumberTextFieldTextField(sender: UITextField!) {
-        if phoneNumberTextField.text?.substringFromIndex((phoneNumberTextField.text?.startIndex)!) == "+" {
-            phoneNumberTextField.text?.removeAtIndex((phoneNumberTextField.text?.startIndex)!)
-        }
-        if let number = Int((phoneNumberTextField.text)!) {
-            phoneNumber = number
-        } else {
-            phoneNumber = nil
-            let alertTwo = UIAlertView()
-            alertTwo.message = "Change the number in order to create an account"
-            alertTwo.addButtonWithTitle("Ok")
-            alertTwo.title = "Not a valid number"
-            alertTwo.show()
-            self.view.endEditing(true)
-        }
-        if let phoneNumberCopy = phoneNumber {
-            print(phoneNumberCopy)
-        }
-    }
+
     
-    func smsSwitchPressed(sender: UISwitch!) {
-        if smsSwitch.on {
-            sms = true
-        } else {
-            sms = false
-        }
-    }
-    
-    func whatsAppSwitchPressed(sender: UISwitch!) {
-        if whatsAppSwitch.on {
-            whatsapp = true
-        } else {
-            whatsapp = false
-            
-        }
-    }
     
     func loginButtonPressed(sender: UIButton!) {
-        if phoneNumberTextField.text?.substringFromIndex((phoneNumberTextField.text?.startIndex)!) == "+" {
-            phoneNumberTextField.text?.removeAtIndex((phoneNumberTextField.text?.startIndex)!)
-        }
-        if let number = Int((phoneNumberTextField.text)!) {
-            phoneNumber = number
-            wrongPhoneNumber = false
-        } else {
-            phoneNumber = nil
-            wrongPhoneNumber = true
-        }
+
         
-        if firstName != nil && phoneNumber != nil && profilePicture != nil {
+        if firstName != nil && profilePicture != nil {
             loginButton.backgroundColor = UIColor ( red: 0.0706, green: 0.1569, blue: 0.3098, alpha: 1.0 )
             // Save UUID in NSUserDefaults
             UUID = UIDevice.currentDevice().identifierForVendor!.UUIDString
@@ -139,7 +85,10 @@ class LoginVC: UIViewController, UINavigationControllerDelegate, UIImagePickerCo
             print(UUID)
             print("CIAO")
             
-            let jsonObject: [String: AnyObject] = ["uuid": UUID, "name": firstNameTextField.text!, "img": "nothingSoFar", "sms": sms, "whatsapp": whatsapp, "number": phoneNumber]
+            imageData = UIImagePNGRepresentation(profilePicture.image!)!
+            
+            let imageText : String = imageData.base64EncodedStringWithOptions(.Encoding64CharacterLineLength)
+            let jsonObject: [String: AnyObject] = ["uuid": UUID, "name": firstNameTextField.text!, "img": "nothing", "number" : "number", "whatsapp" : "0", "sms" : "0"]
             
             Alamofire.request(.POST, "http://napolyglot.com:8080/addscholar", parameters: jsonObject)
                 .responseJSON { response in
@@ -170,26 +119,13 @@ class LoginVC: UIViewController, UINavigationControllerDelegate, UIImagePickerCo
             
             
             
-            imageData = UIImagePNGRepresentation(profilePicture.image!)!
-            
-            
+
             
             userDefaults.setObject(firstName, forKey: "firstName")
-            userDefaults.setObject(phoneNumber, forKey: "phoneNumber")
-            userDefaults.setObject(sms, forKey: "sms")
-            userDefaults.setObject(whatsapp, forKey: "whatsapp")
             userDefaults.setObject(imageData, forKey: "profilePictureData")
             userDefaults.synchronize()
             
             
-        } else if wrongPhoneNumber {
-            self.loginButton.stopLoading()
-            let alertTwo = UIAlertView()
-            alertTwo.message = "Change the number in order to create an account"
-            alertTwo.addButtonWithTitle("Ok")
-            alertTwo.title = "Not a valid number"
-            alertTwo.show()
-            self.view.endEditing(true)
         }
         
     }
@@ -250,29 +186,7 @@ class LoginVC: UIViewController, UINavigationControllerDelegate, UIImagePickerCo
         firstNameTextField.addTarget(self, action: #selector(LoginVC.editingDidEndFirstNameTextFieldTextField(_:)), forControlEvents: UIControlEvents.EditingChanged)
         self.view.addSubview(firstNameTextField)
         
-        phoneNumberTextFieldLabel = UILabel(frame: CGRectMake(0, 0, bounds.width-bounds.width/10, bounds.height/18))
-        phoneNumberTextFieldLabel.text = " Phone number"
-        phoneNumberTextFieldLabel.center = CGPoint(x: bounds.width/2, y: bounds.height/20*10.5)
-        phoneNumberTextFieldLabel.font = UIFont(name: "Avenir-Light", size: 15+getFontSizeAdditionWithDeviceType())
-        phoneNumberTextFieldLabel.textColor = UIColor ( red: 0.8627, green: 0.8824, blue: 0.9294, alpha: 0.8 )
-        phoneNumberTextFieldLabel.textAlignment = .Left
-        self.view.addSubview(phoneNumberTextFieldLabel)
         
-        phoneNumberTextField = UITextField(frame: CGRectMake(0, 0, bounds.width-bounds.width/10, bounds.height/18))
-        phoneNumberTextField.center = CGPoint(x: bounds.width/2, y: bounds.height/20*11.5)
-        phoneNumberTextField.backgroundColor = UIColor ( red: 0.0779, green: 0.1399, blue: 0.267, alpha: 1.0 )
-        phoneNumberTextField.font = UIFont(name: "Avenir-Light", size: 10+getFontSizeAdditionWithDeviceType())
-        phoneNumberTextField.textColor = UIColor ( red: 0.8627, green: 0.8824, blue: 0.9294, alpha: 1.0 )
-        phoneNumberTextField.borderStyle = UITextBorderStyle.RoundedRect
-        phoneNumberTextField.autocorrectionType = UITextAutocorrectionType.No
-        phoneNumberTextField.keyboardType = UIKeyboardType.PhonePad
-        phoneNumberTextField.returnKeyType = UIReturnKeyType.Done
-        phoneNumberTextField.clearButtonMode = UITextFieldViewMode.WhileEditing;
-        phoneNumberTextField.contentVerticalAlignment = UIControlContentVerticalAlignment.Center
-        phoneNumberTextField.layer.borderColor = UIColor ( red: 0.8667, green: 0.8667, blue: 0.8667, alpha: 1.0 ).CGColor
-        phoneNumberTextField.addTarget(self, action: #selector(LoginVC.editingDidBeginPhoneNumberTextFieldTextField(_:)), forControlEvents: UIControlEvents.EditingChanged)
-        phoneNumberTextField.addTarget(self, action: #selector(LoginVC.editingDidEndPhoneNumberTextFieldTextField(_:)), forControlEvents: UIControlEvents.EditingDidEnd)
-        self.view.addSubview(phoneNumberTextField)
         
         forceTouchInformationLabel = UILabel(frame: CGRectMake(0, 0, bounds.width-bounds.width/10, bounds.height/18))
         forceTouchInformationLabel.text = "Creates a 3D Touch shortcut to contact someone nearby"
@@ -282,33 +196,6 @@ class LoginVC: UIViewController, UINavigationControllerDelegate, UIImagePickerCo
         forceTouchInformationLabel.textAlignment = .Center
         //        self.view.addSubview(forceTouchInformationLabel)
         
-        smsLabel = UILabel(frame: CGRectMake(0, 0, bounds.width/4, bounds.height/18))
-        smsLabel.text = "SMS"
-        smsLabel.center = CGPoint(x: bounds.width/8*3, y: bounds.height/20*15)
-        smsLabel.font = UIFont(name: "Avenir-Light", size: 15+getFontSizeAdditionWithDeviceType())
-        smsLabel.textColor = UIColor ( red: 0.8627, green: 0.8824, blue: 0.9294, alpha: 0.8 )
-        smsLabel.textAlignment = .Left
-        self.view.addSubview(smsLabel)
-        
-        smsSwitch = UISwitch(frame: CGRectMake(0, 0, bounds.width/4, bounds.height/18))
-        smsSwitch.center = CGPoint(x: bounds.width/8*5, y: bounds.height/20*15)
-        smsSwitch.on = false
-        smsSwitch.addTarget(self, action: #selector(LoginVC.smsSwitchPressed(_:)), forControlEvents: .ValueChanged)
-        self.view.addSubview(smsSwitch)
-        
-        whatsAppLabel = UILabel(frame: CGRectMake(0, 0, bounds.width/4, bounds.height/18))
-        whatsAppLabel.text = "WhatsApp"
-        whatsAppLabel.center = CGPoint(x: bounds.width/8*3, y: bounds.height/20*16.3)
-        whatsAppLabel.font = UIFont(name: "Avenir-Light", size: 15+getFontSizeAdditionWithDeviceType())
-        whatsAppLabel.textColor = UIColor ( red: 0.8627, green: 0.8824, blue: 0.9294, alpha: 0.8 )
-        whatsAppLabel.textAlignment = .Left
-        self.view.addSubview(whatsAppLabel)
-        
-        whatsAppSwitch = UISwitch(frame: CGRectMake(0, 0, bounds.width/4, bounds.height/18))
-        whatsAppSwitch.center = CGPoint(x: bounds.width/8*5, y: bounds.height/20*16.3)
-        whatsAppSwitch.on = false
-        whatsAppSwitch.addTarget(self, action: #selector(LoginVC.whatsAppSwitchPressed(_:)), forControlEvents: .ValueChanged)
-        self.view.addSubview(whatsAppSwitch)
         
         loginButton = DeformationButton(frame: CGRectMake(firstNameTextField.frame.origin.x, UIScreen.mainScreen().bounds.height-60, bounds.width-bounds.width/10, 40), color: UIColor ( red: 0.0431, green: 0.1255, blue: 0.2745, alpha: 1.0 ))
         
